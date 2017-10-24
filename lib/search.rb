99b755ad65a300d6714860454f4531b102c502ec
andrewr224 <andrew.r224@gmail.com>
2017-10-24 11:46:45 +0300

    Save result into html file

--- lib/search.rb
+++ lib/search.rb
@@ -2,12 +2,19 @@ require 'rest-client'
 
 class Bing
   BASE_URL = 'https://www.bing.com/search?q='
+  FILE = './index.html'
 
   def self.search(query)
-    RestClient.get(BASE_URL + self.prepare(query))
+    result = RestClient.get(BASE_URL + self.prepare(query))
+    File.open(FILE, 'w') do |f|
+      f.puts result
+    end
+    result
   end
 
   def self.prepare(query)
     query.strip.gsub(/\s+/, '+')
   end
 end
+
+Bing.search("Cats are awesome")
